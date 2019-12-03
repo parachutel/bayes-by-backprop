e66490f79d94a8c01d9f703d80aa07bf874e8286
shengli <lisheng@stanford.edu>
2019-12-02 17:21:14 -0800

    argparse

--- run_bbbrnn.py
+++ run_bbbrnn.py
@@ -8,84 +8,88 @@ import codebase.utils as ut
 import data.data_utils as data_ut
 
 # Data
-batch_size = 30
 n_batches = 2000 # only used by dummy data
 
-n_input_steps = 50
-n_pred_steps = 20
-input_feat_dim = 4
-pred_feat_dim = 2
-
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_name', type=str, default='highd')
 # dataset_name = 'dummy{}d'.format(input_feat_dim)
-dataset_name = 'highd'
-
+parser.add_argument('--batch_size', type=int, default=30)
+parser.add_argument('--n_input_steps', type=int, default=50)
+parser.add_argument('--n_pred_steps', type=int, default=20)
+parser.add_argument('--input_feat_dim', type=int, default=4)
+parser.add_argument('--pred_feat_dim', type=int, default=2)
 # Network
-hidden_feat_dim = 100
-
+parser.add_argument('--hidden_feat_dim', type=int, default=100)
 # Model
-cell = 'LSTM'
-BBB = True
-sharpen = True
-likelihood_cost_form = 'gaussian'
+parser.add_argument('--cell', type=str, default='LSTM')
+parser.add_argument('--BBB', action='store_true')
+parser.add_argument('--sharpen', action='store_true')
+parser.add_argument('--likelihood_cost_form', type=str, default='gaussian')
 # likelihood_cost_form = 'mse'
-nlayers = 1
-dropout = 0
-pi = 0.25
-std1 = np.exp(-1)
-std2 = np.exp(-6)
+parser.add_argument('--nlayers', type=int, default=1)
+parser.add_argument('--dropout', type=float, default=0)
+parser.add_argument('--pi', type=float, default=0.25)
+parser.add_argument('--logstd1', type=int, default=-1)
+parser.add_argument('--logstd2', type=int, default=-6)
+
 
 # Train
-dev_mode = False
-training = True
-clip_grad = 5
-lr = 1e-3
-run = 0
-iter_max = 400000
-iter_plot = 2000
-iter_save = 2000
+parser.add_argument('--dev_mode', action='store_true')
+parser.add_argument('--training', action='store_true')
+parser.add_argument('--clip_grad', type=int, default=5)
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--run', type=int, default=0)
+parser.add_argument('--iter_max', type=int, default=400000)
+parser.add_argument('--iter_plot', type=int, default=2000)
+parser.add_argument('--iter_save', type=int, default=2000)
+
+args = parser.parse_args()
+
+std1 = np.exp(args.logstd1)
+std2 = np.exp(args.logstd2)
 
 # # automatic
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 gpu = False if device == torch.device('cpu') else True
 
 # Enforced settings:
-if not BBB:
-    sharpen = False
+if not args.BBB:
+    args.sharpen = False
     layout = [
-        ('model={:s}', cell),
-        ('BBB={:s}', str(BBB)),
-        ('data={:s}', dataset_name),
-        ('nlayers={:d}', nlayers),
-        ('nhid={:d}', hidden_feat_dim),
-        ('dropout={:.1f}', dropout),
-        ('clipgrad={}', str(clip_grad)),
-        ('loss={:s}', likelihood_cost_form),
-        ('run={:d}', run),
+        ('model={:s}', args.cell),
+        ('BBB={:s}', str(args.BBB)),
+        ('data={:s}', args.dataset_name),
+        ('nlayers={:d}', args.nlayers),
+        ('nhid={:d}', args.hidden_feat_dim),
+        ('dropout={:.1f}', args.dropout),
+        ('clipgrad={}', str(args.clip_grad)),
+        ('loss={:s}', args.likelihood_cost_form),
+        ('run={:d}', args.run),
     ]
 else:
     layout = [
-        ('model={:s}', cell),
-        ('BBB={:s}', str(BBB)),
-        ('data={:s}', dataset_name),
-        ('nlayers={:d}', nlayers),
-        ('nhid={:d}', hidden_feat_dim),
-        ('dropout={:.1f}', dropout),
-        ('clipgrad={}', str(clip_grad)),
-        ('loss={:s}', likelihood_cost_form),
-        ('sharpen={:s}', str(sharpen)),
-        ('pi={:.2f}', pi),
-        ('logstd1={:d}', int(np.log(std1))),
-        ('logstd2={:d}', int(np.log(std2))),
-        ('run={:d}', run),
+        ('model={:s}', args.cell),
+        ('BBB={:s}', str(args.BBB)),
+        ('data={:s}', args.dataset_name),
+        ('nlayers={:d}', args.nlayers),
+        ('nhid={:d}', args.hidden_feat_dim),
+        ('dropout={:.1f}', args.dropout),
+        ('clipgrad={}', str(args.clip_grad)),
+        ('loss={:s}', args.likelihood_cost_form),
+        ('sharpen={:s}', str(args.sharpen)),
+        ('pi={:.2f}', args.pi),
+        ('logstd1={:d}', args.logstd1),
+        ('logstd2={:d}', args.logstd2),
+        ('run={:d}', args.run),
     ]
 
 model_name = '_'.join([t.format(v) for (t, v) in layout])
 print(model_name, '\n')
 
-if not dev_mode:
+if not args.dev_mode:
     ut.prepare_dirs(model_name, overwrite_existing=True)
 else:
-    iter_plot = np.inf
+    args.iter_plot = np.inf
 
 # training_set = data_ut.dummy_data_creator(
 #         batch_size=batch_size, 
@@ -98,34 +102,35 @@ else:
 
 training_set = data_ut.read_highd_data(
     'highd_processed_tracks01-60_fr05_loc123456_p0.30', 
-    batch_size, device)
+    args.batch_size, device)
+n_batches = len(training_set)
 
 model = BBBTimeSeriesPredModel(
-        num_rnn_layers=nlayers,
-        pi=pi,
+        num_rnn_layers=args.nlayers,
+        pi=args.pi,
         std1=std1,
         std2=std2,
         gpu=gpu,
-        BBB=BBB,
-        training=training,
-        sharpen=sharpen,
-        dropout=dropout,
-        likelihood_cost_form=likelihood_cost_form,
-        input_feat_dim=input_feat_dim,
-        pred_feat_dim=pred_feat_dim,
-        hidden_feat_dim=hidden_feat_dim,
-        n_input_steps=n_input_steps,
-        n_pred_steps=n_pred_steps,
-        rnn_cell_type=cell,
+        BBB=args.BBB,
+        training=args.training,
+        sharpen=args.sharpen,
+        dropout=args.dropout,
+        likelihood_cost_form=args.likelihood_cost_form,
+        input_feat_dim=args.input_feat_dim,
+        pred_feat_dim=args.pred_feat_dim,
+        hidden_feat_dim=args.hidden_feat_dim,
+        n_input_steps=args.n_input_steps,
+        n_pred_steps=args.n_pred_steps,
+        rnn_cell_type=args.cell,
         name=model_name,
         device=device).to(device)
 
-train(model, training_set, batch_size, n_batches, 
+train(model, training_set, args.batch_size, n_batches, 
         kernel=data_ut.sinusoidal_kernel,
-        lr=lr,
-        clip_grad=clip_grad,
-        iter_max=iter_max, 
-        iter_save=iter_save, 
-        iter_plot=iter_plot, 
+        lr=args.lr,
+        clip_grad=args.clip_grad,
+        iter_max=args.iter_max, 
+        iter_save=args.iter_save, 
+        iter_plot=args.iter_plot, 
         reinitialize=False)
 
